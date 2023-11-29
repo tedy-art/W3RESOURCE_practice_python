56a718271ae17b050cf16951a0e4ba5b344b1170
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2023-11-29 13:32:08 +0530

    add codes

---
+++ Basic - part ll/2_create_random_string.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program that creates all possible strings using the letters
+ 'a', 'e', 'i', 'o', and 'u'. Ensure that each character is used only once.
+"""
+
+import random
+char_list = ['a', 'e', 'i', 'o', 'u']
+random.shuffle(char_list)
+print(''.join(char_list))
---
+++ Basic - part ll/3_remove_3_pos_number.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program that removes and prints every third number from
+a list of numbers until the list is empty.
+If I have a list[1, 2, 3, 4, 5, 6]
+I want to remove third number value until a list is empty.
+List[1, 2, 3, 4, 5, 6] remove third position value 4
+list[]
+"""
